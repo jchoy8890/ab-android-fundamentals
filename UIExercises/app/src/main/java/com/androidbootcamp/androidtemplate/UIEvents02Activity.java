855c74587cf0bbaae3e6cffb1cc795e428439d1f
Eduardo Medina <emedinaa@gmail.com>
2018-02-26 18:56:43 -0500

    Added samples

---
+++ UIExercises/app/src/main/java/com/androidbootcamp/androidtemplate/UIEvents02Activity.java
@@ -0,0 +1,13 @@
+package com.androidbootcamp.androidtemplate;
+
+import android.os.Bundle;
+
+public class UIEvents02Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uievents02);
+        enabledBack();
+    }
+}
---
+++ UIExercises/app/src/main/java/com/androidbootcamp/androidtemplate/UIEvents04Activity.java
@@ -0,0 +1,13 @@
+package com.androidbootcamp.androidtemplate;
+
+import android.os.Bundle;
+
+public class UIEvents04Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uievents04);
+        enabledBack();
+    }
+}
